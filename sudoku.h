953767377df0fea296948c23af055cc098ea4009
Tom Mitcheson <tm21@sprite08.doc.ic.ac.uk>
2021-10-18 14:26:28 +0100

    With guards on now

--- sudoku.h
+++ sudoku.h
@@ -1,3 +1,6 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
 void load_board(const char* filename, char board[9][9]);
 void display_board(const char board[9][9]);
 bool is_complete(const char board[9][9]);
@@ -5,3 +8,5 @@ bool make_move(const char position[3], int digit, char board[9][9]);
 bool check_input(const char input[3]);
 bool check_logic(const char position[3], int digit, const char board[9][9]);
 void update_board(const char position[3], int digit, char board[9][9]);
+
+#endif
